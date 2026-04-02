efe497589038860286be7cd102ba7fd5d9bb26fb
rukibhamz <53100544+rukibhamz@users.noreply.github.com>
2026-04-02 14:58:57 +0100

    Update edit.php

--- application/views/entities/edit.php
+++ application/views/entities/edit.php
@@ -8,61 +8,61 @@ $page_title = $page_title ?? 'Edit Entity';
 
 <div class="card">
     <div class="card-body">
-        <form method="POST" action="<?= base_url('entities/edit/' . $Entity['id']) ?>">
+        <form method="POST" action="<?= base_url('entities/edit/' . $entity['id']) ?>">
             <?php echo csrf_field(); ?>
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="name" class="form-label">Entity Name <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="name" name="name" value="<?= htmlspecialchars($Entity['name']) ?>" required>
+                    <input type="text" class="form-control" id="name" name="name" value="<?= htmlspecialchars($entity['name']) ?>" required>
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="tax_id" class="form-label">Tax ID</label>
-                    <input type="text" class="form-control" id="tax_id" name="tax_id" value="<?= htmlspecialchars($Entity['tax_id'] ?? '') ?>">
+                    <input type="text" class="form-control" id="tax_id" name="tax_id" value="<?= htmlspecialchars($entity['tax_id'] ?? '') ?>">
                 </div>
             </div>
             <div class="mb-3">
                 <label for="address" class="form-label">Address</label>
-                <input type="text" class="form-control" id="address" name="address" value="<?= htmlspecialchars($Entity['address'] ?? '') ?>">
+                <input type="text" class="form-control" id="address" name="address" value="<?= htmlspecialchars($entity['address'] ?? '') ?>">
             </div>
             <div class="row">
                 <div class="col-md-4 mb-3">
                     <label for="city" class="form-label">City</label>
-                    <input type="text" class="form-control" id="city" name="city" value="<?= htmlspecialchars($Entity['city'] ?? '') ?>">
+                    <input type="text" class="form-control" id="city" name="city" value="<?= htmlspecialchars($entity['city'] ?? '') ?>">
                 </div>
                 <div class="col-md-4 mb-3">
                     <label for="state" class="form-label">State</label>
-                    <input type="text" class="form-control" id="state" name="state" value="<?= htmlspecialchars($Entity['state'] ?? '') ?>">
+                    <input type="text" class="form-control" id="state" name="state" value="<?= htmlspecialchars($entity['state'] ?? '') ?>">
                 </div>
                 <div class="col-md-4 mb-3">
                     <label for="zip_code" class="form-label">Zip Code</label>
-                    <input type="text" class="form-control" id="zip_code" name="zip_code" value="<?= htmlspecialchars($Entity['zip_code'] ?? '') ?>">
+                    <input type="text" class="form-control" id="zip_code" name="zip_code" value="<?= htmlspecialchars($entity['zip_code'] ?? '') ?>">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-4 mb-3">
                     <label for="country" class="form-label">Country</label>
-                    <input type="text" class="form-control" id="country" name="country" value="<?= htmlspecialchars($Entity['country'] ?? '') ?>">
+                    <input type="text" class="form-control" id="country" name="country" value="<?= htmlspecialchars($entity['country'] ?? '') ?>">
                 </div>
                 <div class="col-md-4 mb-3">
                     <label for="phone" class="form-label">Phone</label>
-                    <input type="text" class="form-control" id="phone" name="phone" value="<?= htmlspecialchars($Entity['phone'] ?? '') ?>">
+                    <input type="text" class="form-control" id="phone" name="phone" value="<?= htmlspecialchars($entity['phone'] ?? '') ?>">
                 </div>
                 <div class="col-md-4 mb-3">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" value="<?= htmlspecialchars($Entity['email'] ?? '') ?>">
+                    <input type="email" class="form-control" id="email" name="email" value="<?= htmlspecialchars($entity['email'] ?? '') ?>">
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="website" class="form-label">Website</label>
-                    <input type="url" class="form-control" id="website" name="website" value="<?= htmlspecialchars($Entity['website'] ?? '') ?>">
+                    <input type="url" class="form-control" id="website" name="website" value="<?= htmlspecialchars($entity['website'] ?? '') ?>">
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="currency" class="form-label">Currency</label>
                     <select class="form-select" id="currency" name="currency">
                         <?php
                         $currencies = get_all_currencies();
-                        $currentCurrency = $Entity['currency'] ?? 'USD';
+                        $currentCurrency = $entity['currency'] ?? 'USD';
                         foreach ($currencies as $code => $name): ?>
                             <option value="<?= $code ?>" <?= $code === $currentCurrency ? 'selected' : '' ?>>
                                 <?= $code ?> - <?= $name ?>
